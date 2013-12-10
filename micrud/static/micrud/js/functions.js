fddcb09cdf8f3ebd87dff1f2f7fd568873f7fbd1
Leo Vega <leovp17@gmail.com>
2013-12-10 11:38:22 -0300

    Micrud funcionando con Ajax

---
+++ micrud/static/micrud/js/functions.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+
+
+        $("#addContact").click(function(e) { // for each edit contact url
+            e.preventDefault(); // prevent navigation
+            var url = $(this).data("form"); // get the contact form url
+            $("#contactForm").load(url);
+            return false; // prevent the click propagation
+        });
+
+        $('#contactForm').on('submit', 'form', function () {
+
+            //alert(csrftoken);
+            $.ajax({
+                type: "POST",
+
+                dataType: "json",
+
+                url: $(this).attr('action'),
+
+                data: $(this).serialize(),
+
+                success: function(data, textStatus) { // on success..
+                    console.log(data.status);
+                    console.log(data.data);
+                    if (data.status=="novalido")
+                    {
+                        $("#contactForm").html(data.data);
+                    }
+                    console.log("bien")
+                    $("#contactForm").remove();
+
+                    reloadContacts();
+                },
+
+                error: function() { // on error..
+                    console.log("cueck")
+
+
+                }
+            });
+            return false;
+        });
+
+        function reloadContacts(){
+
+            var urlContacts = $(location).attr('href'); // get the contact list url
+
+            $("#content .table").load("/micrud/ .table .row");
+
+        };
+
+
+
+    });
